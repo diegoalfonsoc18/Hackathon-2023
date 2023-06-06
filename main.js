b060bb2cf2b5d5cad9a2c0d021f9d520128317fe
Diego Alfonso <dlalfonso57@hotmail.com>
2023-06-06 16:57:48 -0500

    repair section-information

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const mobileMenu = document.querySelector(".menu-hamburger");
 const iconHamburger = document.querySelector(".icon-hamburger");
 
 iconHamburger.addEventListener("click", toggleIconMenu);
-
 function toggleIconMenu() {
   mobileMenu.classList.toggle("inactive");
 }
